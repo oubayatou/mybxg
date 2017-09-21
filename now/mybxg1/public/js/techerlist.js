4b514694293310ac6073b5612a5c8e11a2189e8d
oubayatou <1356192536@qq.com>
2017-09-21 16:21:48 +0800

    完成讲师注销启用功能

--- now/mybxg1/public/js/techerlist.js
+++ now/mybxg1/public/js/techerlist.js
@@ -9,6 +9,31 @@ define(['jquery','template'],function ($,template) {
             /* 渲染数据 */
             var teacherTpl = template('teacherTpl',{list:data.result});
             $('#teacherInfo').html(teacherTpl);
+            $('.eod').on('click',function () {
+                var that = this;
+                /* 获取改行的td */
+                var td = $(this).closest('td');
+                var tcId = td.attr('data-tId');
+                var tcStatus = td.attr('data-status');
+                console.log(tcId,tcStatus);
+                $.ajax({
+                    url: '/api/teacher/handle',
+                    type:'post',
+                    data:{tc_id:tcId,tc_status:tcStatus},
+                    dataType: 'json',
+                    success: function (data) {
+                        if(data.code == 200){
+                            td.attr('data-status',data.result.tc_status);
+                            if(data.result.tc_status == 1){
+                                $(that).text('注 销');
+                            }else{
+                                $(that).text('启 用');
+                            }
+                        }
+                    }
+                    
+                });
+            });
         }
     });
 });
