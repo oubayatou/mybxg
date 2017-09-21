77f838282dba3d3614fd8ac212f8c5748916c0b3
oubayatou <1356192536@qq.com>
2017-09-21 22:57:48 +0800

    编辑和添加页面的提交功能

--- now/mybxg1/public/js/teacher-add.js
+++ now/mybxg1/public/js/teacher-add.js
@@ -20,12 +20,30 @@ define(['jquery','template','util'],function ($,template,util) {
                 if(data.code == 200){
                     var html = template('teacherTpl',data.result);
                     $('#teacherInfo').html(html);
+                    submitForm('/api/teacher/update');
                 }
             }
         });
+
     }else {
     //    添加讲师信息
         var html = template('teacherTpl',{operate:'添加讲师'});
         $('#teacherInfo').html(html);
+        submitForm('/api/teacher/add');
+    }
+    function submitForm(url) {
+        $('#teacherBtn').click(function () {
+            $.ajax({
+                type:'post',
+                url: url,
+                data:$('#teacherForm').serialize(),
+                dataType:'json',
+                success:function (data) {
+                    if(data.code == 200){
+                        location.href = '/teacher/list';
+                    }
+                }
+            });
+        });
     }
 });
