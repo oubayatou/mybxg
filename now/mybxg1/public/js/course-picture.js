faea5aa67ee6ae0cc7c47fa378bcad20ec1b72af
oubayatou <1356192536@qq.com>
2017-09-26 10:26:42 +0800

    完成课程图片上传操作

--- now/mybxg1/public/js/course-picture.js
+++ now/mybxg1/public/js/course-picture.js
@@ -1,5 +1,37 @@
-define(['jquery','template','util'],function ($,template,util) {
+define(['jquery','template','util','uploadify'],function ($,template,util) {
     /* 设置导航选中效果 */
     util.setMenu('/course/course_add');
-    
+    /* 获取课程id */
+    var csId = util.qs('cs_id');
+    /* 获取课程图片信息 */
+    $.ajax({
+        type: 'get',
+        url:'/api/course/picture',
+        data:{cs_id:csId},
+        dataType:'json',
+        success:function (data) {
+            // console.log(data);
+            /* 解析数据 渲染页面 */
+            var html = template('pictureTpl',data.result);
+            $('#pictureInfo').html(html);
+            /* 处理封面上传 */
+            $('#myfile').uploadify({
+                swf:'/public/assets/uploadify/uploadify.swf',
+                uploader:'/api/uploader/cover',
+                width:80,
+                height:'auto',
+                itemTemplate:'<span></span>',
+                buttonText:'选择图片',
+                buttonClass:'btn btn-success btn-sm',
+                fileObjName:'cs_cover_original',
+                formData:{cs_id:csId},
+                onUploadSuccess:function (a,b) {
+                    console.log(b);
+                    var obj = JSON.parse(b.trim());
+                    // console.log(obj);
+                    $('.preview img').attr('src',obj.result.path);
+                }
+            })
+        }
+    });
 });
